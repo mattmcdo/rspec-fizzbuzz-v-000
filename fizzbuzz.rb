4754c2de63fea93b64abae07a6d6cb2a4630dbf6
Matthew McDonald <matthew.mcd1031@gmail.com>
2018-08-25 18:40:25 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(num)
   if num % 3 == 0
     "Fizz"
-  elsif
-  num % 5 == 0
-      "Buzz"
-    else
-      num % 15 == 0
-      "FizzBuzz"
+  if num % 5 == 0
+    "Buzz"
+  if num % 3 || 5 == 0
+    "FizzBuzz"
+  else
+    nil
   end
 end
